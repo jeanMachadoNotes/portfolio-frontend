92428cf38568e3daddcc91addc7cbac3055756dc
jeanMachadoNotes <machadonote@gmail.com>
2026-02-18 10:55:08 -0600

    Added new project "rag-bjj-assistant"

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
         name: "Portfolio Website",
         description: "Personal portfolio built with React and FastAPI",
         tech: ["React", "FastAPI"],
-        featured: true
+        featured: false
     },
     {
         name:"Flick Facts - Movie Search",
@@ -12,12 +12,20 @@ const projects = [
         featured: true
     },
     {
+        name:"Brazilian Jiu-Jitsu Knowledge Assistant",
+        description: "A web-based assistant that answers Brazilian Jiu-Jitsu questions using retrieval-augmented generation (RAG).",
+        details: "Allows users to ask technique and training questions and receive grounded repsonses based on curated BJJ knowledge source.",
+        tech: ["React","FastAPI","OpenAI Embeddings","Cosine Similarity","Rate Limiting"],
+        live: "https://rag-bjj-assistant.vercel.app",
+        github: "https://github.com/jeanMachadoNotes/rag-bjj-assistant",
+        featured: true
+    },
+    {
         name:"Other Project",
         description: "Smaller, experimental project.",
         tech: ["Javascript"],
         featured: false
-    },
-    
+    }   
 ];
 
 export default projects;
